780f8ff115b6ae37e3929f39ce1381a18c2f176b
Andreas Nicklaus <andreas_nicklaus@web.de>
2022-08-12 17:45:59 +0200

    fixed path error

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,12 @@
+console.log(process.env.NODE_ENV)
+
 module.exports = {
+    publicPath: process.env.NODE_ENV === "production" ? "/mywebsite/" : "/",
     devServer: {
         port: 12345,
         headers: {
             'Access-Control-Allow-Private-Network': true,
             'Access-Control-Allow-Origin': '*',
         },
-        publicPath: process.env.NODE_ENV === "production" ? "/mywebsite/": "/"
     },
 }
